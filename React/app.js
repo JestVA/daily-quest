c4f9090650c0b6a9c9ebd8c69eb6aa52121e0310
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-11-30 21:51:48 +0000

    First working prototype. Although, I am loading script directly using the script tag. This was a workaround to make Babel transpile. I am trying a v2 where the code which generates the React component is separated in another file.

--- React/app.js
+++ React/app.js
@@ -1,13 +1,15 @@
+
+
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       itemCount: 0
     };
-    // change code below this line
+    // typing it this way makes a lot of sense
     this.addItem =
     this.addItem.bind(this)
-    // change code above this line
+    // binding "this" on a class method in the constructor
   }
   addItem() {
     this.setState({
@@ -20,8 +22,10 @@ class MyComponent extends React.Component {
         { /* change code below this line */ }
         <button onClick={this.addItem}>Click Me</button>
         { /* change code above this line */ }
-        <h1>Current Item Count: {this.state.itemCount}</h1>
+        <h1>Finger trained: {this.state.itemCount}</h1>
       </div>
     );
   }
 };
+
+ReactDOM.render(<MyComponent />, document.querySelector('#MyComponent'))
