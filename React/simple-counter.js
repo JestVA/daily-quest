a9734489ab19bb65b4f3a0e926e3478db7d408cf
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-10 23:04:23 +0000

    creating a simple counter component in React. (a more stateful component)

---
+++ React/simple-counter.js
@@ -0,0 +1,45 @@
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    // change code below this line
+// binding methods to the keyword this in the constructor function of our object
+    this.increment=
+    this.increment.bind(this)
+
+    this.decrement=
+    this.decrement.bind(this)
+
+    this.reset=
+    this.reset.bind(this)
+    // change code above this line
+  }
+  // change code below this line
+
+  // Write the functions...
+
+increment() {
+  this.setState({
+    count: this.state.count + 1
+  })
+}
+
+decrement() {
+  this.setState({
+    count: this.state.count - 1
+  })
+}
+  // change code above this line
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+};
