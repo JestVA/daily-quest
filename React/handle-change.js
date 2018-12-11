7a6365589261c22f6fd35c513499a5a8cbee1e0b
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-11 00:57:15 +0000

    prepared exercises in advance for today

---
+++ React/handle-change.js
@@ -0,0 +1,35 @@
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // change code below this line
+
+    // change code above this line
+  }
+  // change code below this line
+
+  // change code above this line
+  render() {
+    return (
+      <div>
+        { /* change code below this line */}
+
+        { /* change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+
+/*
+First, create a method called handleChange() that has a parameter called event. When the method is called, it receives an event object that contains a string of text from the input element. You can access this string with event.target.value inside the method. Update the input property of the component's state with this new string.
+
+In the render method, create the input element above the h4 tag. Add a value attribute which is equal to the input property of the component's state. Then add an onChange() event handler set to the handleChange() method.
+
+When you type in the input box, that text is processed by the handleChange() method, set as the input property in the local state, and rendered as the value in the input box on the page. The component state is the single source of truth regarding the input data.
+
+Last but not least, don't forget to add the necessary bindings in the constructor.
+*/
---
+++ algebra-intuition.js
@@ -0,0 +1,14 @@
+/*
+FIND 3^a = ?
+given that 9^a = 25
+
+logic: 
+Apply square root to both sides
+
+sqrt(9^a) = sqrt(25)
+sqrt(3^2^a) = 5
+sqrt(3^a^2) = 5
+3^a =5
+
+*/
+
