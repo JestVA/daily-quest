f3b8f4031b01be479c0d328fc1a362770ad6a3d7
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-14 22:31:38 +0200

    Creating a form in React

---
+++ React/react-form.js
@@ -0,0 +1,36 @@
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    // change code below this line
+
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          { /* change code below this line */ }
+
+          { /* change code above this line */ }
+          <button type='submit'>Submit!</button>
+        </form>
+        { /* change code below this line */ }
+
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
