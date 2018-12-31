5b64233125651d824e399f9c0faea85c171f27a7
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-31 16:37:48 +0000

    edit in place

---
+++ js-core/edit-in-place.js
@@ -0,0 +1,12 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+}
+editInPlace();
+
+// what a good example that showcases the use of index to access and override an arrays elements even though declared with const
