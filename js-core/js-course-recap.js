8844e3c190cb1347b8ebff4318d08b43c2ac1d84
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-02 14:03:28 +0000

    // function to add and remove an item and use JSON.stringify

--- js-core/js-course-recap.js
+++ js-core/js-course-recap.js
@@ -15,3 +15,13 @@ function thisIsLocalScope() {
 }
 thisIsLocalScope();
 console.log(myConst);
+// function to add and remove an item and use JSON.stringify
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift();
+}
+const testArr = [1, 2, 3, 4, 5]
+console.log(JSON.stringify(testArr))
+console.log(nextInLine(testArr, 'new value'))
+console.log('After: ' + JSON.stringify(testArr))
+
