b767e7050b1c11eac759d5128065158c76f5035b
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-02 22:17:49 +0000

    destructuring of nested object and some file renaming

--- js-core/__misc.js
+++ js-core/__misc.js
@@ -34,6 +34,22 @@ const x = [1, 2, 3, 4, 5];
 console.log(...x);
 
 // Use Destructuring Assignment to Assign Variables from Nested Objects
+const a = {
+  start: { x: 5, y: 6},
+  end: { x: 6, y: -9 }
+};
+const { start : { x: startX, y: startY }} = a;
+console.log(startX, startY); // 5, 6
+// or like this...
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  const { tomorrow : { max: maxOfTomorrow }} = forecast; // destructuring of nested object
+  return maxOfTomorrow;
+}
 
 // Use Destructuring Assignment to Assign Variables from Arrays
 
