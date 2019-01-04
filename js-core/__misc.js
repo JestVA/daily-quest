6339e69f295b3fbf8a1f609233d582311b4dd4f1
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-04 11:43:33 +0000

    create strings using template literals (and accessing values from object with values as arrays

--- js-core/__misc.js
+++ js-core/__misc.js
@@ -107,6 +107,23 @@ console.log(half(stats)); // should be 28.015
 
 // Create Strings using Template Literals
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+const makeList = (arr) => {
+  const resultDisplayArray = arr.map(e => `<li class="text-warning">${e}</li>`);
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
+
 // Write Concise Object Literal Declarations Using Simple Fields
 
 // Write Concise Declarative Functions with ES6
