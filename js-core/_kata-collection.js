cebcd1437785aec554e3ada72d9b3f4a42a69c58
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-05 10:41:10 +0000

    same kata, solution that uses matched RegEx and understanding the boolean test better

--- js-core/_kata-collection.js
+++ js-core/_kata-collection.js
@@ -9,5 +9,19 @@ const areYouPlayingBanjo = name => {
         return `${name} does not play banjo`
     }
   }
-// other solutions
+
+  // other solutions
+
 const areYouPlayingBanjo = name => name +  (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + ' banjo'
+
+// include regular expression 
+
+'Dorin'.match(/^d/i) // evaluates if both 'd' or 'D' are the first letter of string
+const areYouPlayingBanjo = n => n+(n.match(/^r/i) ? ' plays' : ' does not play') + ' banjo';
+
+/*
+understanding the logic behind regex better:
+
+('dorin'.match(/^d/i) == 'd') == true  
+true // evaluates to true 
+*/
