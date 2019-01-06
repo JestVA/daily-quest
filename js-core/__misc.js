eb1c709dd58b1c1894836116024178ef80673279
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-06 17:20:18 +0000

    use negated character set using ^

--- js-core/__misc.js
+++ js-core/__misc.js
@@ -257,6 +257,9 @@ let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/ig; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
 // Match Single Characters Not Specified
+let quoteSample = "3 blind mice.";
+let myRegex = /[^0-9aeiou]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
 // Match Characters that Occur One or More Times
 // Match Characters that Occur Zero or More Times
 // Find Characters with Lazy Matching
