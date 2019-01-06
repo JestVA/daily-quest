8dfb0e0efe3e838315e048ddefc11449a0bcce87
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-06 16:47:41 +0000

    match literal strings

--- js-core/__misc.js
+++ js-core/__misc.js
@@ -221,6 +221,9 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 // Match Literal Strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
 // Match a Literal String with Different Possibilities
 // Ignore Case While Matching
 // Extract Matches
