d34f3d1a0bb0faffe09a0832f2629faed67a31d5
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-06 16:06:40 +0000

    using import to load function from module,

--- js-core/__misc.js
+++ js-core/__misc.js
@@ -196,6 +196,10 @@ console.log(temp)
 
 // Understand the Differences Between import and require
 
+import { capitalizeString } from 'string_functions'
+
+console.log(capitalizeString("hello!"));
+
 // Use export to Reuse a Code Block
 
 // Use * to Import Everything from a File
