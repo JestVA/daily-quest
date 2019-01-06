9a371166955ebc0ef0cbe2af070c707228f257da
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-06 17:04:50 +0000

    using character classes to match single character with multiple posibilities (i.e. /ig : i = capitalization, g = multiple occurence)

--- js-core/__misc.js
+++ js-core/__misc.js
@@ -245,6 +245,9 @@ let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 let result = unRegex.test(exampleStr);
 // Match Single Character with Multiple Possibilities
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
 // Match Letters of the Alphabet
 // Match Numbers and Letters of the Alphabet
 // Match Single Characters Not Specified
