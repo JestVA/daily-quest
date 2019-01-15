c254049e7408ba5faa82a1a0eb17c78c9184d7e7
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-15 22:22:02 +0000

    green multiply + abstractization of callback function

--- js-core/callback-functions.js
+++ js-core/callback-functions.js
@@ -3,9 +3,10 @@
 const addNumbers = (num1, num2) => num1 + num2;
 const multiplyNumbers = (a, b) => a * b;
 
-const coolFunction = (num1, num2, addNumbers) => {
-  console.log(addNumbers(num1, num2));
+const coolFunction = (num1, num2, callback) => {
+  console.log(callback(num1, num2));
 }
 coolFunction(1, 2, addNumbers);
+coolFunction(2, 7, multiplyNumbers);
 
 
