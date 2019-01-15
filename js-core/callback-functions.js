337b514f33e0d494ca2dd5107c43bdd08f549673
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-15 22:19:53 +0000

    addNumbers green

--- js-core/callback-functions.js
+++ js-core/callback-functions.js
@@ -1,11 +1,11 @@
 // Get up to speed with callback functions 
 
-const addNumbers = (a, b) => a + b;
+const addNumbers = (num1, num2) => num1 + num2;
 const multiplyNumbers = (a, b) => a * b;
 
-const coolFunction = (num1, num2, doSomething) => {
-  console.log(doSomething(num1, num2));
+const coolFunction = (num1, num2, addNumbers) => {
+  console.log(addNumbers(num1, num2));
 }
-coolFunction(addNumbers(1, 2));
+coolFunction(1, 2, addNumbers);
 
 
