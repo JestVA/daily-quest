cdc40b157f9e00ebf8566683fea70c275a70a00c
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-15 22:11:41 +0000

    red callback fn

--- js-core/callback-functions.js
+++ js-core/callback-functions.js
@@ -1,1 +1,11 @@
 // Get up to speed with callback functions 
+
+const addNumbers = (a, b) => a + b;
+const multiplyNumbers = (a, b) => a * b;
+
+const coolFunction = (num1, num2, doSomething) => {
+  console.log(doSomething(num1, num2));
+}
+coolFunction(addNumbers(1, 2));
+
+
