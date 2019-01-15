10ce03740b39301bb2caca5eb9da087bf9b82a80
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-15 23:02:48 +0000

    green another callback function

--- js-core/callback-functions.js
+++ js-core/callback-functions.js
@@ -1,12 +1,24 @@
 // Get up to speed with callback functions 
 
+// Define some simple functions
 const addNumbers = (num1, num2) => num1 + num2;
 const multiplyNumbers = (a, b) => a * b;
 
+// Write a simple callback function
 const coolFunction = (num1, num2, callback) => {
-  console.log(callback(num1, num2));
+  return callback(num1, num2);
 }
-coolFunction(1, 2, addNumbers);
-coolFunction(2, 7, multiplyNumbers);
+
+// Test callback function
+console.log(coolFunction(1, 2, addNumbers)); // 3
+console.log(coolFunction(2, 7, multiplyNumbers)); // 14
+
+// Add another layer of abstractization
+
+const doMath = (a, b) => {
+  console.log(`${a} and ${b} are here`);
+}
+
+console.log(coolFunction(10, 11, doMath)) // "10 and 11 are here"
 
 
