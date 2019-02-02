89a03feb5fff01380a5dfebdb8d8574b32d5f5d7
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-02 17:03:18 +0000

    // Make an object by taking property names from an array of strings

---
+++ how-javascript-works/how-functions-work.js
@@ -0,0 +1,9 @@
+// Make an object by taking property names from an array of strings
+
+function make_set(array, value = true) {
+    const object = Object.create(null);
+    array.forEach(function(name) {
+        object[name] = value;
+    });
+    return object;
+}
