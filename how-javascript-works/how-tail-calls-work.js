842db6acb346fba71af13bc2a33b1569243eac75
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-03 12:37:13 +0000

    implement same function as a recursive tail call

--- how-javascript-works/how-tail-calls-work.js
+++ how-javascript-works/how-tail-calls-work.js
@@ -4,5 +4,14 @@ function factorial(n) {
     if (n < 2) {
         return 1;
     }
-    return n * factorial(n - 1);
+    return n * factorial(n - 1);               // not a tail call
+}
+
+// Recursive function as a tail call
+
+function factorial(n, result = 1) {
+    if (n < 2) {
+        return result;
+    }
+    return factorial(n - 1, n * result)        // tail call
 }
