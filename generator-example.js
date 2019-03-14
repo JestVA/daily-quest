d7ec45ce4677baf989c03056cf04367493d6608c
Dorin <d.dumitrascuta@gmail.com>
2019-03-14 22:41:25 +0000

    note to self: remember that you can return a comparison statement that evaluates to true or false without the need for extra ternary or if statements

--- generator-example.js
+++ generator-example.js
@@ -9,3 +9,6 @@ function generateRange(min, max, step) {
     }
   }
 
+/*  
+* remember you can return a comparison that evaluates directly to a boolean without the need for extra ternary or if statements 
+*/
