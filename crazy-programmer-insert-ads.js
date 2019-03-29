4c716cc4a40a4613d415b50a095bc450786c46ad
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-03-29 16:03:40 +0000

    use prototype to create custom version of traditional Js function and solve a kata

--- crazy-programmer-insert-ads.js
+++ crazy-programmer-insert-ads.js
@@ -1,13 +1,32 @@
 // https://www.codewars.com/kata/crazy-programmer-02-insert-advertising-code/train/javascript
 
 // Solution: 
+// This is a great example how to use prototypes to solve things.
+// Write your own custom implementation
 
-function isPrime(value) {
-    for(var i = 2; i < value; i++) {
-        if(value % i === 0) {
-            return false;
-        }
+
+var ads=
+"\nNow is the time for advertisements:\n"+
+"My name is myjinxin, 25 years old, Male, \n"+
+"unmarried, handsome, rich, looking for\n"+
+"a lovely woman as a life partner\n";
+
+
+Number.prototype.toString = function () {
+  if (isPrime(this)) {
+    return ads + this
+  } 
+  
+  return String(+this)
+
+  function isPrime(value) {
+    for (let i = 2; i < value; i++) {
+      if (value % i === 0) {
+        return false 
+      }
     }
-    return value > 1;
+    return value > 1
+  } 
+  
 }
 
