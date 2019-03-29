5a8e8e44a9d9946488cd902f7b678cf8fec0c0ff
Dorin <d.dumitrascuta@gmail.com>
2019-03-29 08:30:12 +0000

    hope to remember this time how I can create an extra parameter in a function and do some calculations within it with the rest of the parameters

--- bmi.js
+++ bmi.js
@@ -11,23 +11,32 @@ if bmi > 30 return "Obese"
 */
 
 const bmi = (w, h) => {
-
-	const result = { w / h**h } 
-	return bmi.tag(result) || bmi.tag(default)
-
+    const result = w / Math.pow(h, 2) 
+    return result <= 18.5 ? bmi.tag[1] : 
+           result <= 25.0 ? bmi.tag[2] :  
+           result <= 30 ? bmi.tag[3] :
+           bmi.tag["default"]
 }  
 
-
-
-
-
-
 bmi.tag = {
-	<= 18.5: "Underweight",
-	<= 25.0: "Normal",
-	<= 30.0: "Overweight",
+	1: "Underweight",
+	2: "Normal",
+	3: "Overweight",
 	default: "Obese"
 }
+
+// In this kata I wanted to use an object with key-value pairs as the tests for "ifs". Only managed to get the structure I still ended up using conditional ternary operator
+
+/*
+*** This is the default parameter assignment I was looking for.
+
+I was trying to make it in the "h" parameter. As it turns out you need to create
+an extra one  (like bmi here) where you do the calculations. 
+
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
+*/
 
 
 
