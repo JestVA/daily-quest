1c382bba35946c821e7e761b27d5b10acf362204
Dorin <d.dumitrascuta@gmail.com>
2019-04-02 23:47:14 +0100

    remember to spread strings in arrays

--- number-rearranged.js
+++ number-rearranged.js
@@ -6,3 +6,13 @@ function superSize(num){
 }
 
 // What I learned: reverse() method of array
+
+// Remember looping through array with for .. of 
+for (let x of arr) {}
+
+// Learn to use trick of empty array 
+const lowercaseCount = str => (str.match(/[a-z]/g) || []).length;
+
+// Remember this function 
+// 1. first, you spread the string into an array ( no need for split )
+const uniTotal = str => [...str].reduce((acc, char) => acc + char.charCodeAt(0), 0);
