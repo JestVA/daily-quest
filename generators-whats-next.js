f1a8f3112d667137d15075b6df4cf5bfb6afe71b
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-04-05 13:36:06 +0100

    play with the generator functions

--- generators-whats-next.js
+++ generators-whats-next.js
@@ -1,33 +1,30 @@
 const nextItem = (xs, item) => {
       
-    if (Array.isArray(xs)) {
-      if (xs.indexOf(item) > -1 && xs.length > 1) {
-              return xs[xs.indexOf(item) + 1]
-      } 
- 
-    } else if (typeof xs === 'string') {
-      const handleString = xs.slice(xs.search(item) + 1, xs.search(item) + 2)
-      return handleString ? handleString : undefined
-    
-    } else if (typeof xs === 'object') {
-              
-        let generator = xs
-        
-        if (generator.next() === undefined) {
-          return undefined
-        } else {
-          return generator.next().value + item
+  if (Array.isArray(xs)) {
+    if (xs.indexOf(item) > -1 && xs.length > 1) {
+            return xs[xs.indexOf(item) + 1]
+    } 
 
-        }
-        
-//           let total = 0
-//           for (let i = 0; i <= item; i++) {
-//             total = generator.next(item).value
-//           }  
-        
-//        return total
+  } else if (typeof xs === 'string') {
+    const handleString = xs.slice(xs.search(item) + 1, xs.search(item) + 2)
+    return handleString ? handleString : undefined
+  
+  } else if (typeof xs === 'object') {
+            
+      let iterator = xs
+      
+      if (iterator.next() === undefined) {
+        return undefined
+      } else {
+        return iterator.next().value + item
 
-    }
-    
-    return undefined 
+      }
+      
+
+
+  }
+  
+  return undefined 
 }
+
+// Best attempt: pass 14 tests, fail 4 tests
