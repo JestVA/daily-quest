35201ce4d6e55e88f9e7c82c13245c4bb041eefd
Dorin <d.dumitrascuta@gmail.com>
2019-07-19 18:24:06 +0100

    creating a merge objects function and calling it with two objects to see the final result: an object holding 2 objects

--- merge-object.js
+++ merge-object.js
@@ -7,3 +7,6 @@ const clonedObj1 = { ...obj1 }
 const mergedObj = { ...obj1, ...obj2 }
 // object{ foo:undefined, x:true, y:{} }
 
+const merge = (...objects) => ({ ...objects })
+
+const mergedObjectFromFunction = merge(obj1, obj2)
