d7feac0012429cb037c9a846663a1c4793c877ae
Dorin <d.dumitrascuta@gmail.com>
2019-07-19 19:08:53 +0100

    refactoring

--- iterables [symbol.iterator].js
+++ iterables [symbol.iterator].js
@@ -139,3 +139,25 @@ VM3098:2 Z
 undefined
 
 */
+
+let range = {
+    from: 1,
+    to: 5,
+  
+    [Symbol.iterator]() {
+      this.current = this.from;
+      return this;
+    },
+  
+    next() {
+      if (this.current <= this.to) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    }
+  };
+  
+  for (let num of range) {
+    alert(num); // 1, then 2, 3, 4, 5
+  }
