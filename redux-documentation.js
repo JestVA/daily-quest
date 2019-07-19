d60bd49c798272fb813da7bde57a665491f1f442
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-07-19 09:58:32 +0100

    correct typo in default switch case

--- redux-documentation.js
+++ redux-documentation.js
@@ -8,13 +8,19 @@ const ADD_TODO = 'ADD_TODO'
 
 // this is an action, plain {}
 // must always have type
+
+/*
 {
 	type: ADD_TODO,
 	text: 'Discover Redux'
 }
+*/
+
+
 
 import { ADD_TODO, REMOVE_TODO } from '../actionTypes'
 
+/*
 {
 	type: TOGGLE_TODO,
 	index: 5
@@ -25,6 +31,7 @@ import { ADD_TODO, REMOVE_TODO } from '../actionTypes'
 	filter: SHOW_COMPLETED
 }
 
+*/
 
 // action creator - they return actions
 function addTodo(text) {
@@ -72,7 +79,7 @@ function todoApp(state = initialState, action) {
 			return Object.assign({}, state, {
 				visibilityFilter: action.filter
 			})
-		default;
+		default:
 			return state 
 	}
 }
@@ -81,7 +88,7 @@ const todoApp = (state = initialState, action) => {
 	switch (action.type) {
 		case SET_VISIBILITY_FILTER:
 			return { ...state, visibilityFilter: action.filter }
-		default;
+		default:
 			return state
 	}
 } 
