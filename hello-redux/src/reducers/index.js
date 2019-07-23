e5bd57ee08f133948f138c1badcf65d7e4a5bfdb
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-07-23 11:21:08 +0100

    add the boilerplate switch case to the reducer

--- hello-redux/src/reducers/index.js
+++ hello-redux/src/reducers/index.js
@@ -1,4 +1,10 @@
 export default (state, action) => {
+    switch (action.type) {
+        case 'SET_TECHNOLOGY':
+            // do something
+        default:
+            return state
+
+    }
     console.log(action)
-    return state
 }
