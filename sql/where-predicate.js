e0e9ace9b570492b7928e59d4f89cf90abe3aa25
Dorin <d.dumitrascuta@gmail.com>
2019-07-27 18:36:20 +0100

    add cheatsheet examples of using where predicates

---
+++ sql/where-predicate.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Cheatsheet
+ * (can be used with WHERE predicates)
+ * 
+ * age >= 21
+ * isDeleted != TRUE
+ * temperature BETWEEN 68 AND 75
+ * companyname IN ('Microsoft', 'Linkedin')
+ * email LIKE '%.gov'
+ * summary LIKE '%spicy%'
+ * billing_state LIKE '__'
+ * 
+ */
