775e599ffe8520e577ddd042c764d599f2c25035
Dorin <d.dumitrascuta@gmail.com>
2019-07-28 22:47:38 +0100

    using path and os modules

--- node/module-function.js
+++ node/module-function.js
@@ -7,3 +7,17 @@ console.log(__dirname)
 console.log('hi, I\'m the module wrapper function')
 
 // })()
+
+
+const path = require('path')
+const pathObj = path.parse(__filename)
+const pathObjDir = path.parse(__dirname)
+console.log(pathObj)
+console.log(pathObjDir)
+
+// 
+
+const os = require('os')
+const totalMemory = os.totalmem() // impure functions
+const availableMemory = os.freemem() // same, every time a different output
+console.log(`...from ${totalMemory} // ${availableMemory} available or ${ Math.floor(totalMemory / availableMemory) }% free memory. `)
