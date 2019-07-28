e6cfaf83aae83793aacc963b3188e78809075b60
Dorin <d.dumitrascuta@gmail.com>
2019-07-28 22:22:58 +0100

    module wrapper function arguments

---
+++ node/module-function.js
@@ -0,0 +1,9 @@
+
+
+// (function (exports, require, module, __filename, __dirname){
+
+console.log(__filename)
+console.log(__dirname)
+console.log('hi, I\'m the module wrapper function')
+
+// })()
