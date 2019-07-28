5ceafad8754d21e5753d31397aad5c462d93c3ba
Dorin <d.dumitrascuta@gmail.com>
2019-07-28 22:01:27 +0100

    or without destructuring the module, getting just the object

--- node/import.js
+++ node/import.js
@@ -1,5 +1,13 @@
 const { extractMatch, pollSomeAPI, endpoint } = require('./every-file-is-module')
 
+// Without destructurin you can get the entire exports object
+
+/*
+const methodsAPI = require('./every-file-is-module')
+methodsAPI.endpoint
+methodsAPI.extractMatch
+methodsAPI.pollSomeAPI
+*/
 
 console.log(`Calculating ID from ${endpoint}`)
 console.log(`Getting result... id is: ${ extractMatch(endpoint) }`) // careful to return the result
