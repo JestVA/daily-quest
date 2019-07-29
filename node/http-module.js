54cb64ea64419a955e47640533da51b1f1d7250a
Dorin <d.dumitrascuta@gmail.com>
2019-07-29 15:01:23 +0100

    use response methhod and a callback function directly nin the createServer class to write a response sending out JSON objects to a created route

--- node/http-module.js
+++ node/http-module.js
@@ -1,10 +1,22 @@
 const http = require('http')
 
-const server = http.createServer(); // this server is an event emitter
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World')
+        res.end()
+    }
 
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([{type: true, valid: 'yes'}, {type: false, valid: 'no'}, {type: null, valid: 'yes'}]))
+        res.end()
+    }
+}); 
+
+/*
 server.on('connection', (socket) => {
     console.log('New connection.')
 }) // registering a handler
+*/
 
 server.listen(3000)
 
