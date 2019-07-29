548489800bdf6b4be11ef45cf38a0e0721a5d044
Dorin <d.dumitrascuta@gmail.com>
2019-07-29 14:31:13 +0100

    add a logger class and instantiate it, playing with the events API in node

---
+++ node/logger.js
@@ -0,0 +1,17 @@
+const EventEmitter = require('events')
+
+const url = 'http://mylogger.io/log'
+
+class Logger extends EventEmitter {
+    log(message) {
+        console.log(message)
+
+        // Raise an event
+        this.emit('messageLogged', {
+            id: 1,
+            url: 'http://'
+        })
+    }
+}   
+
+module.exports = Logger
