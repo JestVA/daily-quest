5b0e4e4101b7ef06ceaa06133b8dea383efc85e9
Dorin <d.dumitrascuta@gmail.com>
2019-08-04 22:58:28 +0100

    same conditional using ternary

--- react-practice/conditional-rendering/Conditional.js
+++ react-practice/conditional-rendering/Conditional.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
 function Conditional(props) {
-    if (props.isLoading === true) {
-        return (
-            <h1>Loading...</h1>
-        )
-    } 
-    return ( // implicit "else"
-        <h1>Loaded!</h1>
+    return (
+        <div>
+            {props.isLoading ? <h1>Loading...</h1> : <h1>Loaded!</h1>}
+        </div>
     )
     
 }
 
 export default Conditional
-// testing for git
