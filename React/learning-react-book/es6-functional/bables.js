323a51311d49d3a8e724de28a1bbb49a81c7fcb9
Dorin <d.dumitrascuta@gmail.com>
2019-08-04 11:20:56 +0100

    object to array and array to object transformations with map and reduce

--- React/learning-react-book/es6-functional/bables.js
+++ React/learning-react-book/es6-functional/bables.js
@@ -36,3 +36,42 @@ const updateSchool = (oldName, name, arr) => {
 let updatedSchools = updateSchool('York', 'Big York', schools)
 console.log(updatedSchools[0])
 console.log(schools[0])
+
+// Transforming an oject into an array:
+let schools2 = {
+     'York': 3 ,
+     'New York': 2
+}
+
+
+const schoolArray = Object.keys(schools2).map(key => ({
+    name: key,
+    wins: schools2[key] 
+}))
+
+console.log(schoolArray)
+
+// Array to Object
+
+const colors = [
+    {
+        id: '-blue',
+        title: 'ocean blue',
+        rating: 2
+    },
+    {
+        id: '-magenta',
+        title: 'chameleon stealth',
+        rating: 10
+    }
+]
+
+const hashColors = colors.reduce(
+    (hash, {id, title, rating}) => {
+        hash[id] = { title, rating }
+        return hash
+    },
+    {} 
+)
+
+console.log(hashColors)
