953567ed98e7d0e26611d6eaa29d07bf347bf091
Dorin <d.dumitrascuta@gmail.com>
2019-08-04 10:47:02 +0100

    calling functions from arrays

---
+++ React/learning-react-book/es6-functional/bables.js
@@ -0,0 +1,8 @@
+// functions can be passed in arrays
+
+const messages = [
+    'functions can be inserted into arrays',
+    (message) => console.log(message)
+]
+
+messages[1](messages[0]) // functions can be inserted into arrays
