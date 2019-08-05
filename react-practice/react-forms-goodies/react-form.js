4487a1e63de41fe99a224ab6ba0445a24d5a78ab
Dorin <d.dumitrascuta@gmail.com>
2019-08-05 13:12:06 +0100

    making it a controlled form by providing the value attribute to the inputs = e.g. single source of truth (what is in state, display in form)

--- react-practice/react-forms-goodies/react-form.js
+++ react-practice/react-forms-goodies/react-form.js
@@ -23,9 +23,9 @@ class App extends Component {
     render() {
         return (
             <form>
-                <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+                <input type="text" value={this.state.firstName} name="firstName" placeholder="First Name" onChange={this.handleChange} />
                 <br />
-                <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+                <input type="text" value={this.state.lastName} name="lastName" placeholder="Last Name" onChange={this.handleChange} />
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
             </form>
         )
