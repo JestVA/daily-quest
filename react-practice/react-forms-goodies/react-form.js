fea1c47266e69473ee4ac8f0a95f11fe2c422b78
Dorin <d.dumitrascuta@gmail.com>
2019-08-05 13:10:12 +0100

    react forms 101

---
+++ react-practice/react-forms-goodies/react-form.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+
+    // we use square brackets because the event target name is a string so this is how you would use a string literal inside an object (just 100% JS)
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    // Having a property afor name inside the target elements makes updating state easy for each of those properties
+
+    render() {
+        return (
+            <form>
+                <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+                <br />
+                <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+        )
+    }
+}
+
+export default App
