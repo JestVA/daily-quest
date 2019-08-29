88d6d88674d23f4e971e1df181f07225aec9c910
dori <d.dumitrascuta@gmail.com>
2019-08-29 21:15:24 +0100

    vs implicit return in one liner functions

--- switch-for-real.js
+++ switch-for-real.js
@@ -29,3 +29,4 @@ function sc(floor){
 // Nice thing here is how you can return something conditionally 
 'Aa~ '.repeat(2) + 'Pa!' + (1<=6 ? ' Aa!': '');
 // When I want to evaluate / return something in JS I should first wrap it in round paranthesis. I do not know why I find it difficult to remember.. prolly because I think about the paranthesis as reserved to Boolean expressions and something does not click
+// Like in one line functions where it is used as implicit return 
