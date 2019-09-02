988f5c62c12f5ba1aa12b4cbf39e9a0fd8c52e12
dori <d.dumitrascuta@gmail.com>
2019-09-02 23:27:05 +0100

    start a new kata [algorithms, numbers]

---
+++ hungry-sequence.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/hungry-sequence/train/javascript
+// numbers, algorithms 
+
+const hungrySequence = n => {
+
+    // start with any n, maybe a low one 
+    
+    // construct the rules i.e. no previous an should be able to divide the new n 
+    // console.log(n)
+    
+    return [n]
+      
+      
+      
+      
+      
+     
+    
+    
+    
+    
+    }
