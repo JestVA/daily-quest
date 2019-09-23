394c09a78963ee5a1a3c25979b09614e9e36253f
dori <d.dumitrascuta@gmail.com>
2019-09-23 22:47:32 +0100

    include tests used solving the kata

--- berlin-clock.js
+++ berlin-clock.js
@@ -66,3 +66,14 @@ const berlinClock = time =>
 			fourthRow;
 
 }
+
+/* INCLUDE TESTS 
+Test.assertSimilar(berlinClock("12:56:01"), 
+"O\nRROO\nRROO\nYYRYYRYYRYY\nYOOO")
+
+Test.assertSimilar(berlinClock("00:00:00"), 
+"Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO")
+
+Test.assertSimilar(berlinClock("22:32:45"), 
+"O\nRRRR\nRROO\nYYRYYROOOOO\nYYOO")
+*/
