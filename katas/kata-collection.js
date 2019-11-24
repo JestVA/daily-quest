1e5794312b1d69248cf27fce2bfef890eab58635
dori <d.dumitrascuta@gmail.com>
2019-11-24 00:09:55 +0000

    format the kata file with beautify

--- katas/kata-collection.js
+++ katas/kata-collection.js
@@ -4,8 +4,18 @@
  */
 
 function isValidWalk(walk) {
-	const mapCoordinates = { n: 1, s: -1, e: 1, w: -1 };
-	const finishPosition = { n: [], s: [], e: [], w: [] };
+	const mapCoordinates = {
+		n: 1,
+		s: -1,
+		e: 1,
+		w: -1
+	};
+	const finishPosition = {
+		n: [],
+		s: [],
+		e: [],
+		w: []
+	};
 
 	walk.forEach(block => finishPosition[block].push(mapCoordinates[block]));
 
@@ -67,88 +77,83 @@ function persistence(num) {
 https://www.codewars.com/kata/snail/train/javascript
  */
 
-const snail = array =>
-{
-   // const action = action.nextMove = (coords) => 
-   // {
-   // 	newCoords = coords.map(coord => )
+const snail = array => {
+	// const action = action.nextMove = (coords) => 
+	// {
+	// 	newCoords = coords.map(coord => )
 
 
-   // 	action.moves.push(newCoords)
-   
-   
-   // }
-   
-   const moveState = {};
-   
-   const stareaInitiala = [1, 1, 1, 1]; // procedure 1 
-   
-   const proceduri = {
-	   "procedura_1": [1, 1, 1, 1],
-	   "procedura_urmatoare": []
-   }
-   
-
-   createAGoodDict = {}
-
-   const flatMatrix = arr.flatMap(x => x);
-   
-   const flatMatrixLength = flatMatrix.length;
-   
-   if(flatMatrixLength === 0)
-	   return [];
-   
+	// 	action.moves.push(newCoords)
 
 
-   const decide = (action) => 
-   {
-	   if(action.type == "procedura_1")
-	   {
-	   
-	   }
+	// }
 
-	   action.nextMove = (coords) => 
-	   {
-		   newCoords = coords.map(coord => )
+	const moveState = {};
 
+	const stareaInitiala = [1, 1, 1, 1]; // procedure 1 
 
-		   action.moves.push(newCoords)
-	   
-	   
-	   }
-	   
-   }
-   // Can be an [] like this:
-   // array = [[1,2,3],[4,5,6],[7,8,9]]
-   // result = [1,2,3,6,9,8,7,4,5]
-
-   
-
-   // simulate the phisics of the snail
-   // it can move only ahead until it finds obstacle and only right when it found obstacle
-   // the start array is always traversed to the max and snail never returns in it
-   
-   // at each new step, snail has 3 choices:
-	   // to a step ahead
-	   // to go a step right
-	   // to stop
-   
-   
-   
+	const proceduri = {
+		"procedura_1": [1, 1, 1, 1],
+		"procedura_urmatoare": []
+	}
 
 
-   
+	createAGoodDict = {}
 
-   const move = (history, position) =>
-   {
+	const flatMatrix = arr.flatMap(x => x);
 
-   }
+	const flatMatrixLength = flatMatrix.length;
+
+	if (flatMatrixLength === 0)
+		return [];
+
+
+
+	const decide = (action) => {
+		if (action.type == "procedura_1") {
+
+		}
+
+		action.nextMove = (coords) => {
+			newCoords = coords.map(coord => )
+
+
+			action.moves.push(newCoords)
+
+
+		}
+
+	}
+	// Can be an [] like this:
+	// array = [[1,2,3],[4,5,6],[7,8,9]]
+	// result = [1,2,3,6,9,8,7,4,5]
+
+
+
+	// simulate the phisics of the snail
+	// it can move only ahead until it finds obstacle and only right when it found obstacle
+	// the start array is always traversed to the max and snail never returns in it
+
+	// at each new step, snail has 3 choices:
+	// to a step ahead
+	// to go a step right
+	// to stop
 
 
 
 
-   // should return an []
-   // return; 
+
+
+
+	const move = (history, position) => {
+
+	}
+
+
+
+
+	// should return an []
+	// return; 
 }
 
 /**
@@ -160,46 +165,43 @@ const snail = array =>
  */
 
 class Thing {
-	constructor(name) 
-	{
+	constructor(name) {
 		this.name = name;
 	}
 
-	set has()
-	{
+	set has() {
 		this.arms = [];
 	}
-	
-	get is_a_woman() 
-	{
-		return this.is_a()
-  	}
-  
-	get is_a_man() {
-	return this.is_not_a()
-  }
-	
-	is_a(){
-	  return true;
-	}
-	
-	is_not_a(){
-	  return false;
-  }
-  
-	get arms() {
-	  return [];
-	}
-	has(quantity){
-	  return this.arms = quantity;
-	}
-	
-  }
 
-  const handler = {
-	  get: (obj, prop) => prop in obj ? obj[prop] : console.log('Not found')
-  }
-  const proxiedClass = new Proxy(class Thing {}, handler);
+	get is_a_woman() {
+		return this.is_a()
+	}
+
+	get is_a_man() {
+		return this.is_not_a()
+	}
+
+	is_a() {
+		return true;
+	}
+
+	is_not_a() {
+		return false;
+	}
+
+	get arms() {
+		return [];
+	}
+	has(quantity) {
+		return this.arms = quantity;
+	}
+
+}
+
+const handler = {
+	get: (obj, prop) => prop in obj ? obj[prop] : console.log('Not found')
+}
+const proxiedClass = new Proxy(class Thing {}, handler);
 
 
 
@@ -207,59 +209,59 @@ class Thing {
 /* 
  5 KYU https://www.codewars.com/kata/54a91a4883a7de5d7800009c
  */
-function incrementString (string) {
+function incrementString(string) {
 	console.log(string)
-  
-  debugger;
-  
-  const regx = /(\d+$)/;
-	
-	const capture = regx.exec(string); 
-	
-	if(!capture)
+
+	debugger;
+
+	const regx = /(\d+$)/;
+
+	const capture = regx.exec(string);
+
+	if (!capture)
 		return string + 1; // nothing to do here but to append a 1 
-	
+
 	// we have a digit somewhere 
 
 	let strDigit = capture[1]; // assignment to constant variable otherwise
-	  
+
 	const num = parseInt(strDigit).toString().length;
-	
-	if(num == strDigit.length)
+
+	if (num == strDigit.length)
 		return string.replace(regx, ++strDigit); // also increments it
-	
+
 	// It's complicated 
 
 	// 001 or 00100
-  
-  // what if 000 ?
-	
+
+	// what if 000 ?
+
 	const filterRegex = /([1-9]+)/;
 
-	const digitBetweenZeros = filterRegex.exec(strDigit);  
+	const digitBetweenZeros = filterRegex.exec(strDigit);
 
-  let indexOfDigitBetweenZeros = null;
-  let moreThanOneDigit = null;
-  
-  if(!digitBetweenZeros)
-    return string.slice(0, string.length - 1) + 1; 
-  
-  
-  if(digitBetweenZeros) // like not null 
-  {
-    indexOfDigitBetweenZeros = digitBetweenZeros.index;
-    
-    moreThanOneDigit = digitBetweenZeros[1].length > 1;  
-  }
-  
-  const justLastOne = parseInt(strDigit.slice(indexOfDigitBetweenZeros)) + 1;
-  
-  const beforeIfAny = strDigit.slice(0, indexOfDigitBetweenZeros)
-  
-  // before if any will need to unshift a 0, if 009 becomes 010 than we just lost a 0
-  
-  if(justLastOne % 10 == 0) // it went from 9 -> 10 or 99 -> 100
-    return string.replace(strDigit, '') + beforeIfAny.slice(1) + justLastOne;
+	let indexOfDigitBetweenZeros = null;
+	let moreThanOneDigit = null;
+
+	if (!digitBetweenZeros)
+		return string.slice(0, string.length - 1) + 1;
+
+
+	if (digitBetweenZeros) // like not null 
+	{
+		indexOfDigitBetweenZeros = digitBetweenZeros.index;
+
+		moreThanOneDigit = digitBetweenZeros[1].length > 1;
+	}
+
+	const justLastOne = parseInt(strDigit.slice(indexOfDigitBetweenZeros)) + 1;
+
+	const beforeIfAny = strDigit.slice(0, indexOfDigitBetweenZeros)
+
+	// before if any will need to unshift a 0, if 009 becomes 010 than we just lost a 0
+
+	if (justLastOne % 10 == 0) // it went from 9 -> 10 or 99 -> 100
+		return string.replace(strDigit, '') + beforeIfAny.slice(1) + justLastOne;
 
 	return string.replace(strDigit, '') + beforeIfAny + justLastOne;
 
