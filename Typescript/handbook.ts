af38e1f962c2c97713276a83535913560a72e44d
Dorin Stefan <d.dumitrascuta@gmail.com>
2021-03-03 08:15:44 +0000

    Add ts handbook, tsconfig and deal with annoying ES lint error w bigint literals

---
+++ Typescript/handbook.ts
@@ -0,0 +1,12 @@
+/*
+	Basic Types
+*/
+
+// Boolean
+let isDone: boolean = false;
+// Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+let big: bigint = 100n;
