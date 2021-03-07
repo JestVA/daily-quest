6571f848ae556414e738627864e2240a310e4c4b
Dorin Stefan <d.dumitrascuta@gmail.com>
2021-03-07 18:21:15 +0000

    Going through lots of nice concepts presented in  TS handbook v2

---
+++ Typescript/no-emit-on-error/hello.ts
@@ -0,0 +1,107 @@
+//const greet = (person, date): void =>
+//	console.log(`Hello ${person}, today is ${date}!`); // today is undefined!
+const greet = (person: string, date: Date): void =>
+	console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+//greet("Dorin"); // compile with tsc --noEmitOnError hello.ts (it will not create hello.js)
+//greet("Dorin", Date()); // Date() returns a string! 
+greet("Dorin", new Date()); // OK!
+
+// these 2 are equivalent, 1 with destructuring one without...
+const functionTakesDestructObjectParam = ({ x, y }: { x: number; y: number }) => console.log(`${x} and ${y}`);
+const functionTakesAPointObjectParam = (pt: { x: number; y: number }) => console.log(`${pt.x} and ${pt.y}`);
+const printOptional = (obj: { firstName: string, surname?: string }) => console.log(`${obj.firstName}${obj.surname}`);
+printOptional({ firstName: 'Dorin' });
+printOptional({ firstName: 'Dorin', surname: "Dumitrascuta" });
+
+// ok so this `optional` symbol notation also works with function parameters not only with optional object properties
+const funcWithOptParam = (p1, p2?) => { };
+funcWithOptParam("justone");
+
+// type alias that is an object with x and y properties
+type Point = {
+	x: number;
+	y?: number; // can be optional
+};
+
+const functionTakesAPointAliasTypeWithOptionalYProperty = (pt: Point) => { return pt };
+
+// can also be an union of types
+type ID = string | number;
+
+const funcTakesID = (id: ID) => console.log(id);
+
+// key difference of interface vs type is that interface can be extended
+interface Pint {
+	alchool: number;
+}
+
+// heuristic: use `interface` unless needing features from `type`
+
+// Literal types
+// useful for combining specific literals into unions
+
+// this could be very good for defining props in React
+function printText(s: string, alignment: "left" | "center" | "right") {
+	//..
+}
+
+printText("Holla", "left");
+printText("Holla", "centre"); // this is mega good for React props..
+
+
+type Distance = number; // nice, could not do the same with interface (which is used more with object shaped type declarations)
+const sorter = (a: Distance, b: Distance): -1 | 0 | 1 => a === b ? 0 : a < b ? -1 : 1;
+
+// like here, width is destructured
+interface Options {
+	width: number
+}
+
+function configure(x: Options | "auto") {
+	return x;
+}
+
+const optionObj1 = {
+	width: 200
+};
+const optionObj2 = {
+	length: 300
+};
+
+configure({ width: 200 }); // OK
+configure(optionObj1); // OK
+configure(optionObj2); // NOT OK
+configure(200);
+configure("auto"); // OK
+configure("automatic"); // NOT OK
+
+// Literal inference (wow, this blew my mind a bit)
+
+const req = {
+	url: "https://abc.com",
+	method: "GET" // this is type String, not type "GET", hence the error below...
+};
+
+const handleRequest = (url: string, method: "GET" | "POST"): void => { };
+
+handleRequest(req.url, req.method); // argument of type string cannot be assigned to argument of type GET | POST
+
+// workarounds
+
+handleRequest(req.url, req.method as "GET"); // I know this is GET... yea...
+
+//or do this 
+const req2 = {
+	url: "https://abc.com",
+	method: "GET" as "GET" // this will always be GET
+};
+
+handleRequest(req2.url, req2.method); // OK, method is now GET
+
+// we can convert an entire object to be of type literals. Nice!!!
+const req3 = {
+	url: "https://abc.com",
+	method: "GET"
+} as const; // ok so now this is now readonly
+
+handleRequest(req3.url, req3.method);
